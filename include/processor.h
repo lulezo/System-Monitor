1be310ce571d89c21ba48c039eab470722911fe9
lulezo <moiz.hassan.mh@gmail.com>
2023-07-15 20:54:08 -0400

    Update processor.h

--- include/processor.h
+++ include/processor.h
@@ -3,10 +3,17 @@
 
 class Processor {
  public:
-  float Utilization();  // TODO: See src/processor.cpp
+  float Utilization(); 
 
-  // TODO: Declare any necessary private members
  private:
+  float prevuser = 0;
+  float prevnice = 0;
+  float prevsystem = 0;
+  float previrq = 0;
+  float prevsoftirq = 0;
+  float prevsteal = 0;
+  float previdle = 0;
+  float previowait = 0;
 };
 
 #endif
