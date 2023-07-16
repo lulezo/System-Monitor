728e6218f73da2cbb6fe9ba757e58e03b1a6232d
lulezo <moiz.hassan.mh@gmail.com>
2023-07-16 00:54:38 -0400

    Update process.h

--- include/process.h
+++ include/process.h
@@ -8,16 +8,15 @@ It contains relevant attributes as shown below
 */
 class Process {
  public:
-  int Pid();                               // TODO: See src/process.cpp
-  void pidSetter(int i);                   // Process's ID setter function
-  std::string User();                      // TODO: See src/process.cpp
-  std::string Command();                   // TODO: See src/process.cpp
-  float CpuUtilization() const;                  // TODO: See src/process.cpp
-  std::string Ram();                       // TODO: See src/process.cpp
-  long int UpTime();                       // TODO: See src/process.cpp
-  bool operator<(Process const& a) const;  // TODO: See src/process.cpp
-
-  // TODO: Declare any necessary private members
+  int Pid();                               
+  void pidSetter(int i);                   
+  std::string User();                      
+  std::string Command();                   
+  float CpuUtilization() const;                  
+  std::string Ram();                       
+  long int UpTime();                       
+  bool operator<(Process const& a) const;  
+ 
  private:
   int pid_;
 };
